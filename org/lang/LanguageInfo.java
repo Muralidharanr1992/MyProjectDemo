29b1becde8608bed783691eaae3ccdab232f227f
Muralidharanr1992 <muralidharanr1992@gmail.com>
2019-11-28 20:12:48 +0530

    First Commit

---
+++ org/lang/LanguageInfo.java
@@ -0,0 +1,17 @@
+package org.lang;
+
+public class LanguageInfo {
+
+	public void tamil() {
+		System.out.println("Tamil is an Language for Tamil Peoples.");
+	}
+	
+	public void hindi() {
+		System.out.println("Hindi is an Language for Hindi Peoples.");
+	}
+	
+	public void english() {
+		System.out.println("English is an Language for All Peoples.");
+	}
+	
+}
